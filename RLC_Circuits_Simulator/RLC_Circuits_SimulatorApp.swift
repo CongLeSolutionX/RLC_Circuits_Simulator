fe6fbd9284938246532c6ae7845c7c8b7cf704ce
Cong Le <longchike@gmail.com>
2025-06-28 01:19:29 -0700

    setup

---
+++ RLC_Circuits_Simulator/RLC_Circuits_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLC_Circuits_SimulatorApp.swift
+//  RLC_Circuits_Simulator
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct RLC_Circuits_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
